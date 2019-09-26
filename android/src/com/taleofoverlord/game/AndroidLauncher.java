beb48576fe481f0979a1901edba90b3c6f1f7cd6
parinya <devilbevilgamer@gmail.com>
2019-09-26 22:10:24 +0700

    Initial commit

---
+++ android/src/com/taleofoverlord/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.taleofoverlord.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.taleofoverlord.game.TaleOfOverlord;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TaleOfOverlord(), config);
+	}
+}
---
+++ desktop/src/com/taleofoverlord/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.taleofoverlord.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.taleofoverlord.game.TaleOfOverlord;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaleOfOverlord(), config);
+	}
+}
