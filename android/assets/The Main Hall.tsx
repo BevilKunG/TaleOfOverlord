ec0dbc585c5a9c9cb3ce070900b03eaa90b467e4
HawKeRian <honong2350@gmail.com>
2019-11-07 21:01:36 +0700

    add new tileset map see it on level3

---
+++ android/assets/The Main Hall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="The Main Hall" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="5964" columns="142">
+ <image source="The Main Hall.png" width="2569" height="769"/>
+</tileset>
