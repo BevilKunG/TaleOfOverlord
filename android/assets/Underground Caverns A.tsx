62da5ecffed21d9c76bb104e1ad52494523f0142
HawKeRian <honong2350@gmail.com>
2019-11-29 13:59:07 +0700

    add many things

---
+++ android/assets/Underground Caverns A.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Underground Caverns A" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="69460" columns="302">
+ <image source="Underground Caverns A.png" width="5436" height="4141"/>
+</tileset>
---
+++ android/assets/rushMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="rushMap" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="4158" columns="99">
+ <image source="Game Boy Advance - Castlevania Harmony of Dissonance - Boss Rush Mode.png" width="1788" height="772"/>
+</tileset>
